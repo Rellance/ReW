aa1d8404e394dfb500f5a64a31aaef39519e0be7
Rellance <ilyanevajno1@gmail.com>
2025-02-25 13:51:57 +0200

    update

--- food/app/Http/Controllers/Admin/CategoryController.php
+++ food/app/Http/Controllers/Admin/CategoryController.php
@@ -8,32 +8,36 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Category;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
+
 class CategoryController extends Controller
 {
-    public function AllCategory(){
+    public function AllCategory()
+    {
         $category = Category::latest()->get();
         return view('admin.backend.category.all_category', compact('category'));
     }
 
-    public function AddCategory(){
+    public function AddCategory()
+    {
         return view('admin.backend.category.add_category');
     }
 
-    public function StoreCategory(Request $request){
+    public function StoreCategory(Request $request)
+    {
 
         if ($request->file('image')) {
             $image = $request->file('image');
             $manager = new ImageManager(new Driver());
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
             $img = $manager->read($image);
-            $img->resize(300,300)->save(public_path('upload/category/'.$name_gen));
-            $save_url = 'upload/category/'.$name_gen;
+            $img->resize(300, 300)->save(public_path('upload/category/' . $name_gen));
+            $save_url = 'upload/category/' . $name_gen;
 
             Category::create([
                 'category_name' => $request->category_name,
-                'image' => $save_url, 
-            ]); 
-        } 
+                'image' => $save_url,
+            ]);
+        }
 
         $notification = array(
             'message' => 'Category Inserted Successfully',
@@ -41,51 +45,62 @@ class CategoryController extends Controller
         );
 
         return redirect()->route('all.category')->with($notification);
-
     }
 
-    public function EditCategory($id){
+    public function EditCategory($id)
+    {
         $category = Category::find($id);
         return view('admin.backend.category.edit_category', compact('category'));
-
     }
 
-    public function UpdateCategory(Request $request){
+    public function UpdateCategory(Request $request)
+    {
 
         $cat_id = $request->id;
 
         if ($request->file('image')) {
             $image = $request->file('image');
             $manager = new ImageManager(new Driver());
-            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
             $img = $manager->read($image);
-            $img->resize(300,300)->save(public_path('upload/category/'.$name_gen));
-            $save_url = 'upload/category/'.$name_gen;
+            $img->resize(300, 300)->save(public_path('upload/category/' . $name_gen));
+            $save_url = 'upload/category/' . $name_gen;
 
             Category::find($cat_id)->update([
                 'category_name' => $request->category_name,
-                'image' => $save_url, 
-            ]); 
+                'image' => $save_url,
+            ]);
             $notification = array(
                 'message' => 'Category Updated Successfully',
                 'alert-type' => 'success'
             );
-    
+
             return redirect()->route('all.category')->with($notification);
         } else {
             Category::find($cat_id)->update([
                 'category_name' => $request->category_name,
-            ]); 
+            ]);
             $notification = array(
                 'message' => 'Category Updated Successfully',
                 'alert-type' => 'success'
             );
-    
+
             return redirect()->route('all.category')->with($notification);
         }
+    }
+    public function DeleteCategory($id)
+    {
+        $item = Category::find($id);
+        $img = $item->image;
+        unlink($img);
+        Category::find($id)->delete();
 
-       
+        $notification = array(
+            'message' => 'Category Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
 
     }
-
 }
--- food/resources/views/admin/dashboard.blade.php
+++ food/resources/views/admin/dashboard.blade.php
@@ -101,9 +101,11 @@
 
     <script src="{{ asset('backend/assets/js/app.js') }}"></script>
     <script src="{{ asset('backend/assets/js/validate.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="{{ asset('backend/assets/js/code.js') }}"></script>
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <!--
+
         <script>
          @if(Session::has('message'))
          var type = "{{ Session::get('alert-type','info') }}"
@@ -126,7 +128,7 @@
          }
          @endif 
         </script>
-        -->
+
 
 </body>
 
