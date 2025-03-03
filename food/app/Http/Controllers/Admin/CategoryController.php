eb190b1f201ea54fd0288e8514f8ebb4b050e5df
Rellance <ilyanevajno1@gmail.com>
2025-03-03 11:22:56 +0200

    City_CRUD

--- food/app/Http/Controllers/Admin/CategoryController.php
+++ food/app/Http/Controllers/Admin/CategoryController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Category;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
+use App\Models\City;
 
 class CategoryController extends Controller
 {
@@ -101,6 +102,65 @@ class CategoryController extends Controller
         );
 
         return redirect()->back()->with($notification);
-
     }
+
+    public function AllCity()
+    {
+        $city = City::latest()->get();
+        return view('admin.backend.city.all_city', compact('city'));
+    }
+
+    public function StoreCity(Request $request){
+
+        City::create([
+               'city_name' => $request->city_name,
+               'city_slug' =>  strtolower(str_replace(' ','-',$request->city_name)), 
+           ]);  
+
+
+       $notification = array(
+           'message' => 'City Inserted Successfully',
+           'alert-type' => 'success'
+       );
+
+       return redirect()->back()->with($notification);
+
+   }
+   // End Method 
+
+   public function EditCity($id){
+       $city = City::find($id);
+       return response()->json($city);
+   }
+   // End Method 
+
+   public function UpdateCity(Request $request){
+    $cat_id = $request->cat_id;
+
+    City::find($cat_id)->update([
+           'city_name' => $request->city_name,
+           'city_slug' =>  strtolower(str_replace(' ','-',$request->city_name)), 
+       ]);  
+
+
+   $notification = array(
+       'message' => 'City Updated Successfully',
+       'alert-type' => 'success'
+   );
+
+   return redirect()->back()->with($notification);
+
+}
+// End Method 
+
+public function DeleteCity($id){
+    City::find($id)->delete();
+
+    $notification = array(
+        'message' => 'City Deleted Successfully',
+        'alert-type' => 'success'
+    );
+
+    return redirect()->back()->with($notification);
+}
 }
